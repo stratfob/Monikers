4b59c807f3561fdd3c8d1035d536e5f5375ae4e7
benzer95 <benstratford586@gmail.com>
2017-09-14 20:52:09 +0100

    Players are now dealt 8 different card ids from the deck

--- app/src/main/java/stratford/monikers/chooseCardsActivity.java
+++ app/src/main/java/stratford/monikers/chooseCardsActivity.java
@@ -3,6 +3,7 @@ package stratford.monikers;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,8 +19,11 @@ public class chooseCardsActivity extends AppCompatActivity {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference gameRef;
+    DatabaseReference playerRef;
+    DatabaseReference cardRef;
     String passedKey;
     String myUsername;
+    int[] cards;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,25 @@ public class chooseCardsActivity extends AppCompatActivity {
         passedKey = intent.getStringExtra("key");
         myUsername = intent.getStringExtra("username");
         gameRef = database.getReference("game").child(passedKey);
+        playerRef = gameRef.child("players");
+        cardRef = gameRef.child("cardsInPlay");
+        cards = new int[485];
+
+        cardRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int j = 0;
+                for(DataSnapshot card : dataSnapshot.getChildren()){
+                    cards[j] = Integer.parseInt(card.getValue()+"");
+                    j++;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
@@ -38,14 +61,20 @@ public class chooseCardsActivity extends AppCompatActivity {
         super.onStart();
 
         // Read from the database
-        gameRef.addValueEventListener(new ValueEventListener() {
+        playerRef.addValueEventListener(new ValueEventListener() {
 
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                DataSnapshot playersSnapshot = dataSnapshot.child("players");
                 int i = 0;
-                for (DataSnapshot playerSnap : playersSnapshot.getChildren()) {
+                TextView text = (TextView) findViewById(R.id.textView);
+                text.setText("");
+                String dealtCards = "";
+                for (DataSnapshot playerSnap : dataSnapshot.getChildren()) {
                     if(playerSnap.getKey().equals(myUsername)){
+                        for(int j=0;j<8;j++){
+                            dealtCards+=cards[i*8 + j] + " ";
+                        }
+                        text.setText(dealtCards);
                         Toast.makeText(chooseCardsActivity.this, "You are player "+i, Toast.LENGTH_SHORT).show();
                         break;
                     }
